18b190216ecffbbed8207a255f2d82693203975b
Pavel Balashov <pavel.balashov1@gmail.com>
2024-09-25 16:50:23 +0200

    feat(app-nest-1,app-angular-1): improve the Azure Web PubSub experiment

--- apps/app-angular-1/src/app/app.component.ts
+++ apps/app-angular-1/src/app/app.component.ts
@@ -22,21 +22,26 @@ export class AppComponent implements OnInit {
   async ngOnInit() {
     const response$ = this.http.get<any>('/api/web-pubsub/token/my-user');
     const response = await firstValueFrom(response$);
-
     console.log('Token response:', response);
-    this.client = new WebPubSubClient(response.url);
 
+    // https://learn.microsoft.com/en-us/azure/azure-web-pubsub/reference-client-sdk-javascript#2-connect-with-your-web-pubsub-resource
+    this.client = new WebPubSubClient(response.url);
     await this.client.start();
 
+    // https://learn.microsoft.com/en-us/azure/azure-web-pubsub/reference-client-sdk-javascript#handle-connected-disconnected-and-stopped-events
     this.client.on('connected', () => {
       console.log('Connected');
     });
+
     this.client.on('disconnected', () => {
       console.log('Disconnected');
     });
+
     this.client.on('stopped', () => {
       console.log('Stopped');
     });
+
+    // https://learn.microsoft.com/en-us/azure/azure-web-pubsub/reference-client-sdk-javascript#a-client-consumes-messages-from-the-application-server-or-joined-groups
     this.client.on('server-message', (message: any) => {
       console.log('Server message', message);
     });
--- apps/app-nest-1/src/web-pubsub/web-pubsub.controller.ts
+++ apps/app-nest-1/src/web-pubsub/web-pubsub.controller.ts
@@ -33,20 +33,79 @@ export class WebPubsubController {
 
   @Post('send')
   @HttpCode(HttpStatus.OK)
-  async sendMessage(): Promise<void> {
+  async sendToHub(): Promise<void> {
     await this.sendMessageToHub('Hello from server');
+  }
+
+  @Post('send/:userId')
+  @HttpCode(HttpStatus.OK)
+  async sendToUser(@Param('userId') userId: string): Promise<void> {
+    if (!userId) {
+      throw new Error('userId is required');
+    }
+
+    await this.sendMessageToUser(userId, 'Hello from server');
   }
 
   private async getClientTokenResponse(
     userId: string,
   ): Promise<ClientTokenResponse> {
-    return this.webPubSubServiceClient.getClientAccessToken({
-      userId,
-    });
+    try {
+      // https://learn.microsoft.com/en-us/azure/azure-web-pubsub/reference-server-sdk-js#get-the-access-token-for-a-client-to-start-the-websocket-connection
+      // https://learn.microsoft.com/en-us/rest/api/webpubsub/dataplane/web-pub-sub/generate-client-token?view=rest-webpubsub-dataplane-2024-01-01&tabs=HTTP
+      return this.webPubSubServiceClient.getClientAccessToken({
+        userId,
+      });
+    } catch (error) {
+      console.error('Failed to get client token:', error);
+      throw error;
+    }
   }
 
   private async sendMessageToHub(message: string): Promise<void> {
-    await this.webPubSubServiceClient.sendToAll(message);
-    console.log('Message sent');
+    try {
+      // https://learn.microsoft.com/en-us/azure/azure-web-pubsub/reference-server-sdk-js#broadcast-messages-to-all-connections-in-a-hub
+      // https://learn.microsoft.com/en-us/rest/api/webpubsub/dataplane/web-pub-sub/send-to-all?view=rest-webpubsub-dataplane-2024-01-01&tabs=HTTP
+      await this.webPubSubServiceClient.sendToAll(message);
+    } catch (error) {
+      console.error('Failed to send message to hub:', error);
+      throw error;
+    }
+
+    console.log('Message sent to hub');
+  }
+
+  private async sendMessageToUser(
+    userId: string,
+    message: string,
+  ): Promise<void> {
+    if (!(await this.isUserConnected(userId))) {
+      throw new Error(`User ${userId} is not connected`);
+    }
+
+    try {
+      // https://learn.microsoft.com/en-us/azure/azure-web-pubsub/reference-server-sdk-js#send-messages-to-all-connections-for-a-user
+      // https://learn.microsoft.com/en-us/rest/api/webpubsub/dataplane/web-pub-sub/send-to-user?view=rest-webpubsub-dataplane-2024-01-01&tabs=HTTP
+      // NOTE: It returns success even if client with specified userId is not connected
+      await this.webPubSubServiceClient.sendToUser(userId, message);
+    } catch (error) {
+      console.error(`Failed to send message to user with ID ${userId}`, error);
+      throw error;
+    }
+
+    console.log(`Message sent to user with ID ${userId}`);
+  }
+
+  private async isUserConnected(userId: string): Promise<boolean> {
+    try {
+      // https://learn.microsoft.com/en-us/rest/api/webpubsub/dataplane/web-pub-sub/connection-exists?view=rest-webpubsub-dataplane-2024-01-01&tabs=HTTP
+      return this.webPubSubServiceClient.userExists(userId);
+    } catch (error) {
+      console.error(
+        `Failed to check user existence for user with ID ${userId}:`,
+        error,
+      );
+      throw error;
+    }
   }
 }
